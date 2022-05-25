8c20beb7a209163749f04afa1cfef6dd616b43b9
dev au <>
2022-05-25 09:27:20 +0700

    list model

---
+++ lib/models/list/list_model.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+
+class ListModel<T> extends Equatable {
+  final int? count;
+  final String? next;
+  final String? previous;
+  final List<T>? results;
+
+  const ListModel({this.count, this.next, this.previous, this.results});
+  factory ListModel.fromJson(Map<String, dynamic> json,
+      T Function(Map<String, dynamic> json) convert) {
+    return ListModel(
+        count: json['count'],
+        next: json['next'],
+        previous: json['previous'],
+        results: (json['results'] as List).map((e) => convert(e)).toList());
+  }
+
+  ListModel<T> copyWith(
+      {int? count, String? next, String? previous, List<T>? results}) {
+    return ListModel(
+      count: count ?? this.count,
+      next: next ?? this.next,
+      previous: previous ?? this.previous,
+      results: results ?? this.results,
+    );
+  }
+
+  @override
+  List<Object?> get props => [count, next, previous, results];
+}
