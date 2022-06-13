0e738ba82f09254950f175950f42ce9e86ad7dfa
dev au <>
2022-06-13 08:27:33 +0700

    customise date picker

--- lib/widgets/widget_date_picker.dart
+++ lib/widgets/widget_date_picker.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart' as Date;
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+
+enum DateType { datePicker, dateTimePicker, timePicker, timePicker24h }
 
 class MyWidgetDatePicker extends StatelessWidget {
   const MyWidgetDatePicker(
@@ -15,18 +17,20 @@ class MyWidgetDatePicker extends StatelessWidget {
       required this.doneStyle,
       required this.child,
       this.theme,
-      this.locale})
+      this.locale,
+      this.dateType = DateType.datePicker})
       : super(key: key);
   final String? date;
-  final Function onPress;
+  final Function(DateTime) onPress;
   final DateTime? initialDate;
   final DateTime? lastDate;
   final DateTime? firstDate;
   final Function? onTap;
   final TextStyle itemStyle, cancelStyle, doneStyle;
   final Widget child;
-  final Date.DatePickerTheme? theme;
-  final Date.LocaleType? locale;
+  final DatePickerTheme? theme;
+  final LocaleType? locale;
+  final DateType dateType;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -34,29 +38,68 @@ class MyWidgetDatePicker extends StatelessWidget {
         if (onTap != null) {
           onTap!();
         }
-        Date.DatePicker.showDatePicker(
-          context,
-          showTitleActions: true,
-          currentTime: initialDate ?? DateTime.now(),
-          locale: locale ?? Date.LocaleType.en,
-          minTime: firstDate ?? DateTime(1900, 1),
-          maxTime: lastDate ?? DateTime(2122, 7),
-          theme: theme ??
-              Date.DatePickerTheme(
-                  headerColor: Colors.white,
-                  backgroundColor: Colors.white,
-                  itemStyle: itemStyle,
-                  cancelStyle: cancelStyle,
-                  doneStyle: doneStyle),
-          onChanged: (date) {
-            print(date);
-          },
-          onConfirm: (date) {
-            onPress(date);
-          },
-        );
+        switch (dateType) {
+          case DateType.datePicker:
+            DatePicker.showDatePicker(
+              context,
+              showTitleActions: true,
+              currentTime: initialDate ?? DateTime.now(),
+              locale: locale ?? LocaleType.en,
+              minTime: firstDate ?? DateTime(1900, 1),
+              maxTime: lastDate ?? DateTime(2122, 7),
+              theme: _theme,
+              onConfirm: _onConfirm,
+            );
+            break;
+          case DateType.dateTimePicker:
+            DatePicker.showDateTimePicker(
+              context,
+              showTitleActions: true,
+              currentTime: initialDate ?? DateTime.now(),
+              locale: locale ?? LocaleType.en,
+              minTime: firstDate ?? DateTime(1900, 1),
+              maxTime: lastDate ?? DateTime(2122, 7),
+              theme: _theme,
+              onConfirm: _onConfirm,
+            );
+            break;
+          case DateType.timePicker:
+            DatePicker.showTimePicker(
+              context,
+              showTitleActions: true,
+              currentTime: initialDate ?? DateTime.now(),
+              locale: locale ?? LocaleType.en,
+              theme: _theme,
+              onConfirm: _onConfirm,
+            );
+            break;
+          case DateType.timePicker24h:
+            DatePicker.showTime12hPicker(
+              context,
+              showTitleActions: true,
+              currentTime: initialDate ?? DateTime.now(),
+              locale: locale ?? LocaleType.en,
+              theme: _theme,
+              onConfirm: _onConfirm,
+            );
+            break;
+          default:
+        }
       },
       child: child,
     );
   }
+
+  _onConfirm(DateTime date) {
+    onPress(date);
+  }
+
+  DatePickerTheme get _theme =>
+      theme ??
+      DatePickerTheme(
+          headerColor: Colors.white,
+          backgroundColor: Colors.white,
+          itemStyle: itemStyle,
+          cancelStyle: cancelStyle,
+          doneStyle: doneStyle);
 }
