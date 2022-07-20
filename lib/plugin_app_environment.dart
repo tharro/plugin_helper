d59a35811a415317b1a59c4bfef9f4bfc3657139
dev au <>
2022-07-20 10:30:53 +0700

    remove type

--- lib/plugin_app_environment.dart
+++ lib/plugin_app_environment.dart
@@ -21,7 +21,7 @@ class MyPluginAppEnvironment {
   String? googleAPIKey;
   String? bucket;
   String? linkCloudfront;
-  Map<String, dynamic>? customKey;
+  Map? customKey;
 
   // Set app configuration with single function
   void setAppConfig({
@@ -34,7 +34,7 @@ class MyPluginAppEnvironment {
     String? googleAPIKey,
     String? bucket,
     String? linkCloudfront,
-    Map<String, dynamic>? customKey,
+    Map? customKey,
   }) {
     this.merchantId = merchantId ?? this.merchantId;
     this.stripePublicKey = stripePublicKey ?? this.stripePublicKey;
