ae3ba2d2b7d9e09c84c4d22a2a127c1ebe61bed1
dev au <>
2022-11-06 21:33:20 +1100

    convert url to file

--- lib/plugin_helper.dart
+++ lib/plugin_helper.dart
@@ -514,21 +514,21 @@ class MyPluginHelper {
     return await _channel.invokeMethod('getLocalTimezone');
   }
 
-  Future<File> urlToFile(String imageUrl) async {
-// generate random number.
+  static Future<File> urlToFile(String imageUrl) async {
+    // generate random number.
     var rng = Random();
-// get temporary directory of device.
+    // get temporary directory of device.
     Directory tempDir = await getTemporaryDirectory();
-// get temporary path from temporary directory.
+    // get temporary path from temporary directory.
     String tempPath = tempDir.path;
-// create a new file in temporary path with random file name.
+    // create a new file in temporary path with random file name.
     File file = File(tempPath + (rng.nextInt(100)).toString() + '.png');
-// call http.get method and pass imageUrl into it to get response.
+    // call http.get method and pass imageUrl into it to get response.
     http.Response response = await http.get(Uri.parse(imageUrl));
-// write bodyBytes received in response to file.
+    // write bodyBytes received in response to file.
     await file.writeAsBytes(response.bodyBytes);
-// now return the file which is created with random name in
-// temporary directory and image bytes from response is written to // that file.
+    // now return the file which is created with random name in
+    // temporary directory and image bytes from response is written to // that file.
     return file;
   }
 }
