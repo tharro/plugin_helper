0c5a671e509f3cabde548c4de1197e8f38574ac7
dev au <>
2023-04-05 22:31:20 +0700

    add app assets

---
+++ lib/config/app_assets.dart
@@ -0,0 +1,6 @@
+class MyPluginAppAssets {
+  final String visa = 'assets/icons/visa.png';
+  final String amex = 'assets/icons/amex.png';
+  final String discovery = 'assets/icons/discovery.png';
+  final String mastercard = 'assets/icons/mastercard.png';
+}
