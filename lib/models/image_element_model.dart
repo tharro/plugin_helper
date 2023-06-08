67ce49d87812c6be1ba43e43b85542b6e1221d4a
dev au <>
2023-06-08 14:24:55 +0700

    update model

--- lib/models/image_element_model.dart
+++ lib/models/image_element_model.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
 import 'dart:typed_data';
 
 class ImageElementModel {
   final String? url;
   final Uint8List? bytes;
+  final File? file;
 
-  ImageElementModel({this.url, this.bytes});
+  ImageElementModel({this.url, this.bytes, this.file});
 }
